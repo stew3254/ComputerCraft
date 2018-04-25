68271da826037cfb470925ccd5bc782acee258bc
Stew <rtstewy@gmail.com>
2018-04-25 18:17:38 -0400

    Fixed Oopsies
    Actually put the cobble code in

--- cobble.lua
+++ cobble.lua
@@ -1,1 +1,17 @@
-shell.run("cobble")
+turtle.select(1)
+while true do
+  if turtle.getItemCount() > 0 then
+    while not turtle.dropUp() do
+      term.clear()
+      term.setCursorPos(1,1)
+      print("Chest is Full!")
+    end
+  end
+  if turtle.detect() then
+    turtle.dig()
+    turtle.suck()
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Filling Chest")
+  end
+end
