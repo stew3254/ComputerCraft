6ca1a908ef9718e4b083640ec50a0bfc5f6d5cc1
Cameron Weinfurt <weinfurt.c@hotmail.com>
2018-10-16 14:22:53 -0400

    Adding documentation

--- thread.lua
+++ thread.lua
@@ -2,6 +2,22 @@
 --
 -- Code for threaded execution of lua code.
 
+<<<<<<< HEAD
+-- Stores all threads that have been created.
+threadParts = {}
+
+-- Creates a new thread
+threadCreate = function(procName, procPath)
+	-- Does the file exist and are we really reading a file?
+	if fs.isDir(procPath) or not fs.exists(procPath) then
+		return false, nil
+	else
+		-- Table that holds all of the functions from the file
+		procTable = {}
+		procFile = fs.open(procPath, "r")
+
+		-- Run through the file by loading functions as strings into procTable.
+=======
 threadParts = {}
 
 threadCreate = function(procName, procPath)
@@ -11,6 +27,7 @@ threadCreate = function(procName, procPath)
 		procTable = {}
 		procFile = fs.open(procPath, "r")
 
+>>>>>>> 2248fe7f7baccc58fa1ad4a7fa70051b06adaff6
 		local functionStr = ""
 		local line, layer
 		repeat
@@ -25,11 +42,20 @@ threadCreate = function(procName, procPath)
 				end
 			end
 		until line ~= nil
+<<<<<<< HEAD
+		
+		-- Create the new thread entry. In order, it contains the name of the process, the path of the file it came from, a table of functions and a table of what functions should be run and what parameters should be passed to it. 
+=======
 
+>>>>>>> 2248fe7f7baccc58fa1ad4a7fa70051b06adaff6
 		threadParts[#threadParts] = {procName, procPath, procTable, procTable[0]}	
 	end
 end
 
+<<<<<<< HEAD
+-- Run through every thread entry and run the queued function. Terminates the thread when there are no more functions to call in procOrder
+=======
+>>>>>>> 2248fe7f7baccc58fa1ad4a7fa70051b06adaff6
 threadIterate = function()
 	for procName, procPath, procTable, procOrder in threadParts do
 		local procOrderEntry = procOrder[procOrder[0]]
@@ -42,6 +68,10 @@ threadIterate = function()
 	end
 end
 
+<<<<<<< HEAD
+-- Called by threadIterate when a thread is out of functions to call. Can also be called to end a thread early. 
+=======
+>>>>>>> 2248fe7f7baccc58fa1ad4a7fa70051b06adaff6
 threadRemove = function(procName)
 	for i,proc in threadParts do
 		if proc[0] == procName then 
